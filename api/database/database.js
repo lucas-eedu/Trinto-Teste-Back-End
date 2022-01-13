8c3b55a2ce99aeb1b776f0394dbf47bd6413a9ad
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-12 22:49:10 -0300

    Database connection

---
+++ api/database/database.js
@@ -0,0 +1,12 @@
+const sequelize = require('sequelize');
+require('dotenv').config();
+
+// Connecting to the database with sequelize
+const connection = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+   host: process.env.DB_HOST,
+   dialect: process.env.DB_DIALECT,
+   dialectModule: require('mysql2'),
+   timezone: process.env.DB_TIMEZONE
+});
+
+module.exports = connection;
